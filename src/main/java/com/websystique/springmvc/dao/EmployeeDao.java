e9757804f4967004ff5aa10ecb5bae776141f21a
Ravi <ravi2krishna@gmail.com>
2017-07-22 11:15:56 +0530

    All files with zip

---
+++ src/main/java/com/websystique/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.websystique.springmvc.dao;
+
+import java.util.List;
+
+import com.websystique.springmvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
